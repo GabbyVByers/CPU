64789a4ab8e6230e9cf2b9675b730b85088e5449
GabbyVByers <gabbyvbyers@gmail.com>
2026-06-06 01:07:04 -0700

    IDE Libraries configured

--- IDE/main.cpp
+++ IDE/main.cpp
@@ -1,8 +1,48 @@
 
+#include <string>
+#include <vector>
 #include <iostream>
 
-int main() {
-	std::cout << "Hellow World!\n";
-	return 0;
+#include "imgui.h"
+#include "imgui-SFML.h"
+
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode({ 1920, 1080 }), "SFML window");
+    if (!ImGui::SFML::Init(window)) {}
+
+    const sf::Font font("JetBrainsMono-Regular.ttf");
+    sf::Text text(font, "Hello SFML", 50);
+
+    sf::Clock clock{};
+    while (window.isOpen()) {
+        while (const std::optional event = window.pollEvent()) {
+            ImGui::SFML::ProcessEvent(window, *event);
+            if (event->is<sf::Event::Closed>())
+                window.close();
+            if (const auto* resized = event->getIf<sf::Event::Resized>()) {
+                sf::FloatRect visibleArea({ 0.f, 0.f }, sf::Vector2f(resized->size));
+                window.setView(sf::View(visibleArea));
+            }
+        }
+
+        ImGui::SFML::Update(window, clock.restart());
+
+        ImGui::Begin("Hello, world!");
+        if (ImGui::Button("Click me!")) {
+            std::cout << "I love being clicked!\n";
+        }
+        ImGui::End();
+
+        window.clear();
+        window.draw(text);
+        ImGui::SFML::Render(window);
+        window.display();
+    }
+
+    ImGui::SFML::Shutdown();
 }
 
